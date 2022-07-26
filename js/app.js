8c153b0adab5758a9b5e397ae543e50ac0b7322c
mteruel28 <106028705+mteruel28@users.noreply.github.com>
2022-07-25 23:20:41 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+//Code to Display Drop Down SideBar Menu
+
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
+
+// Display Mobile Menu
+
+const mobileMenu = () => {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+};
+
+menu.addEventListener("click", mobileMenu);
